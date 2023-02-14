f810dbfb872788f8e9e33399cc2762b9a4bf29df
KKamjangE <ajm980718@gmail.com>
2023-02-15 00:15:24 +0900

    WebSocket Server

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -5,7 +7,13 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000);
+
+const server = http.createServer(app); // http 서버
+
+const wss = new WebSocket.Server({ server }); // webSocket 서버
+
+server.listen(3000, handleListen);

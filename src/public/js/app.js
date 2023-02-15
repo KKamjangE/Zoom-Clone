4282e63c55bc2f4c5a2ceb1cffad6b111f6bde05
KKamjangE <ajm980718@gmail.com>
2023-02-15 23:34:09 +0900

    Room notifications

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,15 +8,46 @@ room.hidden = true;
 
 let roomName;
 
+const addMessage = (message) => {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
+};
+
+const handleMessageSubmit = (event) => {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${input.value}`);
+    input.value = "";
+  });
+};
+
+const showRoom = () => {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
+};
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const input = form.querySelector("input");
   socket.emit("enter_room", input.value, () => {
-    welcome.hidden = true;
-    room.hidden = false;
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName}`;
+    showRoom();
   });
   roomName = input.value;
   input.value = "";
 });
+
+socket.on("welcome", () => {
+  addMessage("someone joined!");
+});
+
+socket.on("bye", () => {
+  addMessage("someone left..");
+});
+socket.on("new_message", addMessage);
